a6671d1d674a84ffca3b30db69e954692d6b76f8
Mubashir <mbnfaheem@gmail.com>
2022-05-16 10:45:24 +0500

    Update ProductDetails.jsx

--- frontend/src/screens/ProductDetails.jsx
+++ frontend/src/screens/ProductDetails.jsx
@@ -111,6 +111,18 @@ const addToCartHandler = ()=>{
                                  ))
                              }
                          </Form.Control>
+{/* 
+<Form.Control
+                  as="select"
+                  value={qty}
+                  onChange={(e) => setQty(e.target.value)}
+                >
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                </Form.Control> */}
 
                      </Row>
                  </ListGroupItem>
