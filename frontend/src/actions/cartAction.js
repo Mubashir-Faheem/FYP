be8565c9a90754658e44e9ee2fd3a2046acb460d
Mubashir-Faheem <92710130+Mubashir-Faheem@users.noreply.github.com>
2022-05-17 01:43:50 +0500

    Add files via upload

---
+++ frontend/src/actions/cartAction.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import {CART_ADD_ITEM,CART_REMOVE_ITEM} from '../constants/cartConstant'
+
+// export const addtoCart=(id,qty)=> async (dispatch,getState)=>{
+//     const {data}=await axios.get(`./api/products/${id}`)
+//     dispatch({
+//         type:CART_ADD_ITEM,
+//         payload:{
+//             product:data._id,
+//             name:data.name,
+//             image:data.image,
+//             price:data.price,
+//             countInStock:data.countInStock,
+//             qty
+//         }
+//     })
+
+    // localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItem))
+
+// }
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+    const { data } = await axios.get(`https://mubashir-garden-mart.herokuapp.com/api/products/${id}`);
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        product: data._id,
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        countInStock: data.countInStock,
+        qty,
+      },
+    });
+  
+    // localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+    // localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  };
+
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+      type: CART_REMOVE_ITEM,
+      payload: id,
+    });
+}
