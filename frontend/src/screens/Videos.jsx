2de43649c63316e3ac2d1db01f897769f9945c83
Mubashir <mbnfaheem@gmail.com>
2022-05-16 09:55:38 +0500

    videos done

--- frontend/src/screens/Videos.jsx
+++ frontend/src/screens/Videos.jsx
@@ -1,10 +1,50 @@
-import React from "react"
+import React from "react";
+import ReactPlayer from "react-player";
+import Card from 'react-bootstrap/Card'
+import { Row,Col } from "react-bootstrap";
+ function Videos() {
 
-const Video = () => {
   return (
-      <>
-<h1>This is video screen</h1>
-</>
-  )
+    <div className="App">
+
+<Row xs={1} md={2} className="g-4">
+  {Array.from({ length: 1 }).map((_, idx) => (
+    <Col>
+      <Card>
+        <Card.Body>
+        <ReactPlayer url="https://www.youtube.com/watch?v=3FGxEYWAPf8" controls={true}/>
+        <ReactPlayer url="https://www.youtube.com/watch?v=eySTo2GgvoY" controls={true}/>
+        </Card.Body>
+        <Card.Body>
+        <ReactPlayer url="https://www.youtube.com/watch?v=KFw5yBYtDnA" controls={true}/>
+        <ReactPlayer url="https://www.youtube.com/watch?v=B0DrWAUsNSc" controls={true}/>
+        </Card.Body>
+      </Card>
+    </Col>
+    
+  ))}
+
+{Array.from({ length: 1 }).map((_, idx) => (
+    <Col>
+      <Card>
+        <Card.Body>
+        <ReactPlayer url="https://www.youtube.com/watch?v=DXwF_jVtwFU" controls={true}/>
+        <ReactPlayer url="https://www.youtube.com/watch?v=eLACnABG2LM" controls={true}/>
+        </Card.Body>
+        <Card.Body>
+        <ReactPlayer url="https://www.youtube.com/watch?v=pLQuIuokP6Q" controls={true}/>
+        <ReactPlayer url="https://www.youtube.com/watch?v=nccV__8Pb9U" controls={true}/>
+        </Card.Body>
+      </Card>
+    </Col>
+    
+  ))}
+
+</Row>
+
+  
+    </div>
+  );
 }
-export default Video
+
+export default Videos
