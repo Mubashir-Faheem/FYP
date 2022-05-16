f1f054045f81a9f840a69404d9d1ccd1312e7ae8
Mubashir <mbnfaheem@gmail.com>
2022-05-17 02:54:28 +0500

    .

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -31,6 +31,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
         qty,
       },
     });
+    console.log(data)
   
     // localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
     // localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
