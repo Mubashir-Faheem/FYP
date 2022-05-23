28f35cd1cf962f97ef05c3b4e4733032b537376b
Mubashir <mbnfaheem@gmail.com>
2022-05-23 20:53:02 +0500

    services admin panel done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,47 +1,3 @@
-// import React from 'react'
-// import Navbar from 'react-bootstrap/Navbar'
-// import Nav from 'react-bootstrap/Nav'
-// import Container from 'react-bootstrap/Container'
-// import {LinkContainer} from "react-router-bootstrap"
-// import {useDispatch,useSelector} from 'react-redux'
-// const header = () => {
-//     return (
-//         <>
-//            <Navbar bg="light" expand="lg" variant="light" collapseOnSelect>
-//   <Container>
-//     <LinkContainer to="/">
-//     <Navbar.Brand >Garden Mart</Navbar.Brand>
-    
-//     </LinkContainer>
-//     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//     <Navbar.Collapse id="basic-navbar-nav">
-//       <Nav className="ml-auto">
-        
-//       <LinkContainer to ="/login">
-//        <Nav.Link >
-//         <i class="fas fa-user-plus"></i>&nbsp; SignIn</Nav.Link>
-//         </LinkContainer>
-//        <LinkContainer to ="/signup">
-//        <Nav.Link >
-//         <i class="fas fa-user-plus"></i>&nbsp; SignUp</Nav.Link>
-//         </LinkContainer>
-//        <LinkContainer to ="/cart">
-//        <Nav.Link >
-//         <i class="fas fa-user-plus"></i>&nbsp; Cart</Nav.Link>
-//         </LinkContainer>
-        
-        
-//       </Nav>
-//     </Navbar.Collapse>
-    
-//   </Container>
-// </Navbar> 
-//         </>
-//     )
-// }
-
-// export default header
-
 
 import React from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
@@ -109,6 +65,9 @@ const Header = () => {
                   </LinkContainer>
                   <LinkContainer to='/admin/bloglist'>
                     <NavDropdown.Item>Blogs</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/rentinglist'>
+                    <NavDropdown.Item>Services</NavDropdown.Item>
                   </LinkContainer>
                 </NavDropdown>
                 
