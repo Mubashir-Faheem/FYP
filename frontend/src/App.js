5ee2bca1944e5f65acf191a70c448a54e343fa4a
Mubashir <mbnfaheem@gmail.com>
2022-05-23 21:15:58 +0500

    extra comments i made for checking,deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,20 @@
 
 import './App.css';
 import Container from "react-bootstrap/Container"
-// import React, { Component }  from 'react';
 import React from 'react'
 import Footer from "./components/footer"
 import Header from "./components/header"
 import Homescreen from "./screens/homescreen"
-// import {ProductShow} from "./screens/ProductScreen"
 import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
-// import ProductScreen from "./screens/ProductScreen"
 import ProductDetails from "./screens/ProductDetails"
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import Forget from './screens/Forget';
 import Main from './screens/Main'
 import SeedDetails from './screens/SeedDetails';
-// import SeedScreen from './screens/SeedScreen';
-// import Post from './screens/Blog/Post'
-// import Posts from './screens/Blog/Posts';
-// import Topbar from './screens/Blog/Topbar';
-// // import SearchBar from './screens/Blog/SearchBar';
-// import SinglePost from './screens/Blog/SinglePost';
-import Fertilizerscreen from './screens/FertilizerScreen';
 import Forum from './screens/Forum';
 import Video from './screens/Videos';
 // import VirtualGarden from './screens/Virtual Garden/Virtual Garden'
-// import SeedScreen from './screens/SeedScreen';
 import SeedHome from './screens/SeedHome'
 import FertilizerHome from './screens/FertilizerHome';
 import FertilizerDetails from './screens/FertilizerDetails'
@@ -51,19 +40,6 @@ import RentingListScreen from './screens/RentingListScreen'
 import RentingEditScreen from './screens/RentingEditScreen';
 
 
-// import Cart from './screens/Cart'
-// import StateProvider from './StateProvider'
-// import  CCartContextProvider from './Contexts/CartContext'
-
-
-// import Register from "./screens/Register/Register"
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom"
-
 
 function App() {
   return (
@@ -71,50 +47,13 @@ function App() {
     <Router>
     <Header/>
     <main className="my-3"><Container>
-      {/* <h1>E-Commerce App</h1> */}
-      {/* <Homescreen /> */}
-     {/* <LoginScreen /> */}
-     {/* <RegisterScreen /> */}
-     {/* {<Main/>} */}
-    {/* <SeedScreen/> */}
-    {/* <Blog/> */}
-    {/* <Fertilizerscreen/> */}
-    {/* <SeedHome/> */}
-    {/* <FertilizerHome/> */}
-    {/* <FertilizerDetails/> */}
-    {/* <CartScreen/> */}
-    {/* <ShippingScreen/> */}
-    {/* <PlaceOrderScreen/> */}
-    {/* <OrderScreen/> */}
-    {/* <RentingHome/> */}
-    {/* <RentingDetails/> */}
-    {/* <UserEditScreen/> */}
-    {/* <UserListScreen/> */}
-    {/* <ProductListScreen/> */}
-    {/* <ProductListScreen/> */}
-    {/* <OrderListScreen/> */}
-    {/* <BlogHome/> */}
-    {/* <BlogDetails/> */}
-    {/* <BlogListScreen/> */}
-    {/* <BlogEditScreen/> */}
-    {/* <RentingListScreen/> */}
-    {/* <RentingEditScreen/> */}
-
-
-
+     
       <Routes >
       <Route path="/" element={<Main />} exact/>
 
       <Route path="/homescreen" element={<Homescreen />} />
-      {/* <Route path="/ProductScreen" element={<ProductScreen />} /> */}
-      {/* <Route path="/seedscreen" element={<SeedScreen />} /> */}
       <Route path="/seedscreen" element={<SeedHome />} />
-      {/* <Route path="/fertscreen" element={<Fertilizerscreen />} /> */}
       <Route path="/fertscreen" element={<FertilizerHome />} />
-      {/* <Route path="/posts" element={<Posts/>} />
-      <Route path="/post" element={<Post/>} />
-      <Route path="/topbar" element={<Topbar/>} />
-      <Route path="/SinglePost" element={<SinglePost/>} /> */}
       <Route path="/product/:id" element ={<ProductDetails />} />
       <Route path="/seeds/:id" element ={<SeedDetails />} />
       <Route path="/fertilizers/:id" element ={<FertilizerDetails />} />
@@ -143,26 +82,9 @@ function App() {
       <Route path="/admin/blog/:id/edit" element={<BlogEditScreen/>} />
       <Route path="/admin/rentinglist" element={<RentingListScreen/>} />
       <Route path="/admin/service/:id/edit" element={<RentingEditScreen/>} />
-
-      
-      <Route
-            path='/admin/productlist'
-            element={<ProductListScreen/>}
-            exact
-          />
-          <Route
-            path='/admin/productlist/:pageNumber'
-            component={<ProductListScreen/>}
-            exact
-          />
-
-
-
-
-      
-      
-     
-   
+      <Route path='/admin/productlist' element={<ProductListScreen/>} exact/>
+      <Route path='/admin/productlist/:pageNumber' element={<ProductListScreen/>} exact />
+  
     </Routes>
     </Container>
     
