168f0a9f2b398c790e1c20ebbe376001bee454ee
Mubashir <mbnfaheem@gmail.com>
2022-05-26 00:34:03 +0500

    final touch

--- backend/data/renting.js
+++ backend/data/renting.js
@@ -2,7 +2,7 @@ const renting =[
     {
     
     name:"Lawn Mowing",
-    image:'images/final.jpg',
+    image:'images/garden-lawn-care-tools-equipment-perfect-green-grass-banner-copy-space-garden-lawn-care-tools-equipment-perfect-193210012.jpg',
     tool:"No extra tool required",
     price:500,
     countinstock:12,
@@ -10,7 +10,7 @@ const renting =[
 },
 {
     name:"Lawn Digging",
-    image:'images/WhatsApp Image 2022-04-08 at 7.24.11 PM.jpeg',
+    image:'images/lawn-care-maintenance-professional-grass-cutting-yard-178538286.jpg',
     tool:"Electronic Digger required",
     price:1000,
     countinstock:12,
@@ -18,7 +18,7 @@ const renting =[
 
 {
     name:"Lawn Cleaning",
-    image:'images/garden-lawn-care-tools-equipment-perfect-green-grass-banner-copy-space-garden-lawn-care-tools-equipment-perfect-193210012.jpg',
+    image:'images/worker-mowing-tall-grass-electric-petrol-lawn-trimmer-city-park-backyard-gardening-care-tools-equipment-process-174293642.jpg',
     tool:"Electronic Leaf Blower required",
     price:1000,
     countinstock:12,
