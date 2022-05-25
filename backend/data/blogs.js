d56c408741661379a44512e5dc7b5b2c61567ae5
Mubashir <mbnfaheem@gmail.com>
2022-05-26 00:37:31 +0500

    Update blogs.js

--- backend/data/blogs.js
+++ backend/data/blogs.js
@@ -3,7 +3,7 @@ const blogs =[
     
     name:"Garden Weeds and Words",
     image:'images/blog.jpg',
-    description:"This blog is a playful look at gardening from all angles by Andrew O’Brien. Recent blog posts have covered gardening clothes (as he posed the question of ‘do you ever garden wearing jeans?’ to his twitter audience) and a blog entitled ‘You snooze, you lose’- which was a fantastic post pointing out that February is NOT a time to rest in your garden.",
+    description:"This blog is a playful look at gardening from all angles by Andrew O’Brien. Recent blog posts have covered gardening clothes (as he posed the question of ‘do you ever garden wearing jeans?’ to his twitter audience) and a blog entitled ‘You snooze, you lose’- which was a fantastic post pointing out that February is NOT a time to rest in your garden.They take awesome photos. Seriously, you need to check out their blog if only for the pictures. But they’re also master designers as well as gardeners, as their backyard oasis looks like a slice of heaven (actually, it might be…).They’re based in the same gardening zone as me, the coveted zone 10b of San Diego, CA and started out as completely fresh gardeners about 8 years ago in 2007, when they moved to the area. Since then their blog has been honored by the New York Times as one of the best gardening blogs online, and I completely agree.It’s awesome and very inspiring. Give it a look – I have it saved in my feeds so I never miss a post.",
 
 },
 
@@ -11,7 +11,7 @@ const blogs =[
     
     name:"Fennel and Fern",
     image:'images/beauty_of_nature_17.jpg',
-    description:"If you’re not seduced by the photography used on this blog site, then you certainly will be by the words. This site is actually a collective, so features blogs from more than one writer. Recent posts have celebrated Hamamelis in the winter garden, as well as suggested ways to inject a contemporary feel into your gardening containers.",
+    description:"If you’re not seduced by the photography used on this blog site, then you certainly will be by the words. This site is actually a collective, so features blogs from more than one writer. Recent posts have celebrated Hamamelis in the winter garden, as well as suggested ways to inject a contemporary feel into your gardening containers.It’s awesome and very inspiring. Give it a look – I have it saved in my feeds so I never miss a post.They’re based in the same gardening zone as me, the coveted zone 10b of San Diego, CA and started out as completely fresh gardeners about 8 years ago in 2007, when they moved to the area. Since then their blog has been honored by the New York Times as one of the best gardening blogs online, and I completely agree.They take awesome photos. Seriously, you need to check out their blog if only for the pictures. But they’re also master designers as well as gardeners, as their backyard oasis looks like a slice of heaven (actually, it might be…).",
 
 },
 
